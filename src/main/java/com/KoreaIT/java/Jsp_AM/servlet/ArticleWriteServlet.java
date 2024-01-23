275d94502eb9ac4ddeab68342f72834c8be78c4a
ksw70913 <pavico0913@gmail.com>
2024-01-23 17:47:52 +0900

    페이지네이션 진행중

--- src/main/java/com/KoreaIT/java/Jsp_AM/servlet/ArticleWriteServlet.java
+++ src/main/java/com/KoreaIT/java/Jsp_AM/servlet/ArticleWriteServlet.java
@@ -14,7 +14,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/article/modify")
+@WebServlet("/article/write")
 public class ArticleWriteServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
